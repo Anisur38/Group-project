7ad6dd82ea60356a16f1c54d6770caaecf22a638
Anisur38 <mdjobair3738@gmail.com>
2026-03-30 04:16:48 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for medicare_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Renders the HTML frontend
-    path('', views.index, name='index'), 
+    path('admin/', admin.site.urls),
     
-    # API endpoints for fetching and posting data
-    path('api/dashboard/', views.get_patient_dashboard_data, name='api_dashboard'),
-    path('api/records/', views.get_medical_records, name='api_records'),
-    path('api/doctors/', views.get_doctors_list, name='api_doctors'),
-    path('api/book/', views.book_appointment, name='api_book'),
+    path('', include('core.urls')), 
 ]
